b51a59c41dcf860110fbf5ff1fe41ad3ef54017c
David Torres <119999924+davzshaw@users.noreply.github.com>
2025-05-31 10:16:18 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+@@ -0,0 +1,28 @@
+# Esta función le corresponde a: alanis
+def suma(a, b):
+    # TODO: implementar la suma
+    pass
+
+print(suma(3, 5))  # Debería imprimir 8
+
+
+# Esta función le corresponde a: sofia
+def saludar(name)
+    print("Hola, " nombre)
+
+saludar("María")
+
+
+# Esta función le corresponde a: mathias
+def contar_hasta(n):
+    for i in range(n):
+        print(i)
+
+contar_hasta(5)  # Esperamos: 1, 2, 3, 4, 5
+
+
+# Esta función le corresponde a: juan felipe
+def mensaje():
+    print("Hola muno")  # Error: falta una letra
+
+mensaje()
